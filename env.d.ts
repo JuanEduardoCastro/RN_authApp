6fba8941574d5b17e5e10a521f71d3de76492e9a
JuanCastroT <juancastrot@JuanCastroTs-MacBook-Pro.local>
2026-08-13 16:27:59 -0300

    CI: add type declarations for env

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+declare module '@env' {
+  export const IOS_CLIENT_ID: string;
+  export const ANDROID_CLIENT_ID: string;
+  export const WEB_CLIENT_ID: string;
+  export const KEY_SERVICES: string;
+  export const GITHUB_CLIENT_ID: string;
+  export const GITHUB_CLIENT_SECRET: string;
+  export const GITHUB_REDIRECT_URI: string;
+  export const API_PUBLIC_KEY_SSL: string;
+  export const API_BACKUP_KEY_SSL: string;
+}
